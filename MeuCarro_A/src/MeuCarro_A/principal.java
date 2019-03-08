e583b3027582fbf5dc13ac833df8ed7d88a96f97
1B Modular Info <1binfo@basilides.local>
2019-03-08 20:14:42 -0300

    inicio do Projeto

---
+++ MeuCarro_A/src/MeuCarro_A/principal.java
@@ -0,0 +1,14 @@
+package MeuCarro_A;
+
+public class principal {
+
+	public static void main(String[] args) {
+		Carro FerrariDe50Cavalos = new Carro();
+
+FerrariDe50Cavalos.setLigado(true);
+
+		FerrariDe50Cavalos.LigarCarro();
+
+	}
+
+}
